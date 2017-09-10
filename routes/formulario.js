ba220c2964ca97f6f28e014303e7de4fe2e7db16
Chiien <fuchienhsu@hotmail.com>
2017-09-10 19:51:26 -0300

    Deleted routes

--- routes/formulario.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-//Injeção de dependência
-const FormularioController = require('../controllers/formulario')()
-
-router.post('/', FormularioController.enviarDados.bind(FormularioController))
-
-module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var router  = express.Router()
 
-router.use('/formulario', require('./formulario'))
 router.use('/ip', require('./ip'))
 router.use('/conversation', require('./conversation'))
 
